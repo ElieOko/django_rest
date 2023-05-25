3fde5cc690d95c1c04975d725a49e83c3f100055
Elie Oko <elieoko100@gmail.com>
2023-05-25 10:14:04 +0100

    feat:change size field in model Students

--- app_student/models.py
+++ app_student/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Students(models.Model):
+
+    list_promo = [("L1-I", "Licence 1 Info"), ("L1-F", "Licence 1 Fisc"), ("L2-C", "Licence 2 Conception"), ("L2-R", "Licence 1 Reseau")]
+    list_genre = [("M", "Monsieur"), ("F", "Madame")]
+    name = models.CharField(max_length=255, unique=True)
+    age = models.IntegerField(default=0)
+    genre = models.CharField(max_length=1, choices=list_genre, default="aucun")
+    promotion = models.CharField(max_length=7, choices=list_promo, default="L1-I")
+    status = models.CharField(max_length=255, default="encours")
+    is_active = models.BooleanField(default=True)
+
+
