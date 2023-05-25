5e9420aa58b9dc3ec26c93b5e6ebbf4427fff5fb
Elie Oko <elieoko100@gmail.com>
2023-05-25 11:21:29 +0100

    feat:add list_method and other fun

--- app_student/view/v_students.py
+++ app_student/view/v_students.py
@@ -1,9 +1,16 @@
 from rest_framework import viewsets
 
 from app_student.models import Students
-from app_student.view.serializers.sz_students import StudentsSerializer
+from app_student.view.serializers.sz_students import StudentsSerializer, UpdateStudentSerializer, \
+    CreateStudentSerializer
 
 
 class StudentsViewSet(viewsets.ModelViewSet):
     queryset = Students.objects.all()
     serializer_class = StudentsSerializer
+    http_method_names = ['get', 'post', 'put', 'patch']
+    lookup_fields = ['name', 'age']
+
+    def get_serializer_class(self):
+        methodes_list = dict(PUT=UpdateStudentSerializer,   POST=CreateStudentSerializer)
+        return methodes_list.get(self.request.method, StudentsSerializer)
