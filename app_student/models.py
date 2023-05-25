316610ffda27776fb89f4e07b00363b592aa73d0
Elie Oko <elieoko100@gmail.com>
2023-05-25 11:19:48 +0100

    feat:change struct in models

--- app_student/models.py
+++ app_student/models.py
@@ -6,11 +6,12 @@ class Students(models.Model):
 
     list_promo = [("L1-I", "Licence 1 Info"), ("L1-F", "Licence 1 Fisc"), ("L2-C", "Licence 2 Conception"), ("L2-R", "Licence 1 Reseau")]
     list_genre = [("M", "Monsieur"), ("F", "Madame")]
+    list_status = [(1, "Encours"), (2, "En attente"), (3, "Terminer")]
     name = models.CharField(max_length=255, unique=True)
     age = models.IntegerField(default=0)
     genre = models.CharField(max_length=1, choices=list_genre, default="aucun")
     promotion = models.CharField(max_length=7, choices=list_promo, default="L1-I")
-    status = models.CharField(max_length=255, default="encours")
+    status = models.IntegerField(choices=list_status, default=1)
     is_active = models.BooleanField(default=True)
 
 
